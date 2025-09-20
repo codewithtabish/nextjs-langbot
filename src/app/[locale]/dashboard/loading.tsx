4bc4836a22692f1966ae68f75993ad8a34204228
coderghoust <tabish169000@gmail.com>
2025-09-20 08:35:41 -1200

    fixed loading file

--- src/app/[locale]/dashboard/loading.tsx
+++ src/app/[locale]/dashboard/loading.tsx
@@ -7,7 +7,7 @@ export default function Loading() {
   return (
     <div className="flex w-screen min-h-screen">
       {/* Sidebar skeleton */}
-      <div className="h-screen w-20 lg:w-64 bg-background/5 border-r shadow-2xl" />
+      {/* <div className="h-screen w-20 lg:w-64 bg-background/5 border-r shadow-2xl" /> */}
 
       {/* Main content skeleton */}
       <main className="flex-1 py-20 px-10 space-y-10">
